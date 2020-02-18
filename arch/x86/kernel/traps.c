08483f69f054cfcd0eb9e9eb925cb705f54d7086
Thomas Gleixner <tglx@linutronix.de>
2020-02-18 22:08:03 +0100

    x86/traps: Prepare for using DEFINE_IDTENTRY
    Prepare for using IDTENTRY to define the C exception/trap entry points. It
    would be possible to glue this into the existing macro maze, but it's
    simpler and better to read at the end to just make them distinct. Provide
    a trivial inline helper to read the trap address.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -274,6 +274,11 @@ static void do_error_trap(struct pt_regs *regs, long error_code, char *str,
 	}
 }
 
+static inline void __user *error_get_trap_addr(struct pt_regs *regs)
+{
+	return (void __user *)uprobe_get_trap_addr(regs);
+}
+
 #define IP ((void __user *)uprobe_get_trap_addr(regs))
 #define DO_ERROR(trapnr, signr, sicode, addr, str, name)		   \
 dotraplinkage void do_##name(struct pt_regs *regs, long error_code)	   \
